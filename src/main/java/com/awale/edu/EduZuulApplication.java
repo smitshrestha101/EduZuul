9b954fd06be2d3c3be732aefbbd6491ba93dcab1
chiragawale <chiragawale777@gmail.com>
2020-09-24 17:16:37 -0500

    Update EduZuulApplication.java

--- src/main/java/com/awale/edu/EduZuulApplication.java
+++ src/main/java/com/awale/edu/EduZuulApplication.java
@@ -2,13 +2,15 @@ package com.awale.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
-//@SpringBootApplication
-//@EnableDiscoveryClient
+@SpringBootApplication
+@EnableDiscoveryClient
 //@EnableZuulProxy
 @Configuration
 @ComponentScan
